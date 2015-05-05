27ca43bbc864516fc8732ca5ba7ad60e64a91d77
Joel Kuiper <me@joelkuiper.eu>
2015-05-05 18:29:38 +0200

    Fix merge mistake

--- resources/public/scripts/app.js
+++ resources/public/scripts/app.js
@@ -27,7 +27,7 @@ define(function (require) {
   var Marginalia = React.createFactory(require("jsx!spa/components/marginalia"));
 
   var process = function(data) {
-    var upload = FileUtil.upload("/topologies/gen2phen", data);
+    var upload = FileUtil.upload("/topologies/ebm", data);
     documentModel.loadFromData(data);
     upload.then(function(result) {
       var marginalia = JSON.parse(result);
